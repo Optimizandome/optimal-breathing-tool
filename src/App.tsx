9b1402cbb1d7d46afaf89c1d9eaf0e8c73b33bcc
Reynaldo Quispe <rquispe@makingsense.com>
2022-04-14 16:10:08 -0400

    fix: change homepage url

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider, Theme } from "theme-ui";
 import ReactGA from "react-ga";
+import { Provider } from "react-redux";
 import "../src/styles/global.css";
 
-import { store } from "./store";
-import { Provider } from "react-redux";
 import { theme } from "styles";
 import { OptimalBreathing } from "layouts";
+import { store } from "./store";
 
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID as string);
 ReactGA.pageview(window.location.pathname + window.location.search);
